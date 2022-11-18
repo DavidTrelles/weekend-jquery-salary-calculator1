6772450f1f6f0fdfa09ea9ffaf3074686a53d1da
David Trelles <davidstrelles@gmail.com>
2022-11-17 20:02:01 -0600

    added css funcitionality if monthlly cost exceeds 20k

--- script.js
+++ script.js
@@ -33,29 +33,34 @@ function newEmployee() {
       employeeArray.push(newEmployee);
     console.log(employeeArray);
   }
-   monthlyCost()
+  monthlyCost();
   renderFunc();
 }
 
-let dollars = Intl.NumberFormat('en-US')
-function monthlyCost(){
-  let total = 0
-  for (let employee of employeeArray){
-    total += (employee.salary/12);
-  } 
-  total = +total
-    $('#costOut').empty().append("$", dollars.format(total))
-} 
+let dollars = Intl.NumberFormat("en-US");
+function monthlyCost() {
+  let total = 0;
+  for (let employee of employeeArray) {
+    total += employee.salary / 12;
+  }
+  total = +total;
+  total = Math.floor(total);
+  $("#costOut").empty().append("$", dollars.format(total));
+  total > 20000
+    ? $("#costOut").css("background-color", "red")
+    : $("#costOut").css("background-color", "white");
+}
+
 
 function renderFunc() {
   let el = $("#newRow");
-   el.empty();
+  el.empty();
 
   for (let employee of employeeArray) {
-  //   let newList = `
-  // <li data-id=${employee.id}>${employee.firstName} ${employee.lastName} ${employee.id} ${employee.title} $${dollars.format(employee.salary)} <button id="removeEmployee">Remove</button></li>
-  // `;
-  let newRow = `
+    //   let newList = `
+    // <li data-id=${employee.id}>${employee.firstName} ${employee.lastName} ${employee.id} ${employee.title} $${dollars.format(employee.salary)} <button id="removeEmployee">Remove</button></li>
+    // `;
+    let newRow = `
   <tr data-id=${employee.id}>
             <td>${employee.firstName}</td>
             <td>${employee.lastName}</td>
@@ -64,7 +69,7 @@ function renderFunc() {
             <td>$${dollars.format(employee.salary)}</td>
             <td><button id="removeEmployee">Remove</button></td>
           </tr>
-  `
+  `;
     el.append(newRow);
   }
 }
@@ -73,8 +78,7 @@ function remove() {
   for (let i = 0; i < employeeArray.length; i++) {
     if (employeeArray[i].id == $(this).parent().parent().data("id"))
       employeeArray.splice(i, 1);
-   console.log($(this).parent().parent().data('id'));
   }
-   monthlyCost();
+  monthlyCost();
   renderFunc();
 }
